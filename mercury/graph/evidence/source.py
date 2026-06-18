dc5ceca9e8ae45ad199e210ef5e871db58ea1b10
sbasaldua <santiago.basaldua@bbvadata.com>
2026-06-18 23:48:16 +0200

    Provides an overview of the Source class

--- mercury/graph/evidence/source.py
+++ mercury/graph/evidence/source.py
@@ -2,8 +2,22 @@ from mercury.graph.evidence import Agentic
 
 
 class Source(Agentic):
+	""" The Source is an Agentic interface to a corpus of documents.
 
+	## Overview
+
+	The Source can contain different types of documents and possibly call format conversion tools.
+	Documents can be text or code.
+
+	The Source provides:
+
+	- A "chunking" interface to break documents into smaller pieces for easier processing.
+	- A hierarchy that divides a corpus into a collection, document, section, paragraph and chunk.
+	- An indexing system that provides unique identifiers for each chunk.
+	- A persistence backend that possibly includes vectorization and embedding of the chunks for later retrieval.
+	- An Agentic interface to everything above.
+
+	"""
 
 	def __init__(self, schema = None, parent = None, logger = None):
 		super().__init__(my_class = 'source', schema = schema, parent = parent, logger = logger)
-
