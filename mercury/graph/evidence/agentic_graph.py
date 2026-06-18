4e7535623f9409d6efe9f7a812f6229c3520f234
sbasaldua <santiago.basaldua@bbvadata.com>
2026-06-18 23:47:04 +0200

    Provides an overview of the AgenticGraph class

--- mercury/graph/evidence/agentic_graph.py
+++ mercury/graph/evidence/agentic_graph.py
@@ -2,8 +2,18 @@ from mercury.graph.evidence import Agentic
 
 
 class AgenticGraph(Agentic):
+	""" AgenticGraph is a class that exposed a `mercury.graph.Graph` in the Agentic tree.
 
+	## Overview
+
+	All the underlying technologies are supported, but typically the graph will be stored in RAM.
+
+	The class also provides functionality to manage entire graphs as an Agentic `schema` providing persistence and concurrency.
+
+	Ontologies, dictionaries and storages that in general will expect key/value stores are just a special case of graphs (without edges).
+	Therefore, this is also the storage for all these things.
+
+	"""
 
 	def __init__(self, schema = None, parent = None, logger = None):
 		super().__init__(my_class = 'agentic_graph', schema = schema, parent = parent, logger = logger)
-
