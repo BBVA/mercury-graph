fbd660a8ab76b4776c79d65630828c501c35568a
sbasaldua <santiago.basaldua@bbvadata.com>
2026-06-18 23:48:03 +0200

    Provides an overview of the Formalizer class

--- mercury/graph/evidence/formalizer.py
+++ mercury/graph/evidence/formalizer.py
@@ -2,8 +2,18 @@ from mercury.graph.evidence import Agentic
 
 
 class Formalizer(Agentic):
+	""" The Formalizer is the class that takes in natural language and produces structured data in the form of subgraphs.
 
+	It uses:
+
+	1. Ontologies for categories of entities and relationships.
+	2. The (SIE) Structured Information Extracting Tools (E.g. GLiNER2).
+	3. It produces subgraphs that can be:
+
+		- Merged into the EvidenceGraph.
+		- Used by the Agent to answer grounded queries.
+
+	"""
 
 	def __init__(self, schema = None, parent = None, logger = None):
 		super().__init__(my_class = 'formalizer', schema = schema, parent = parent, logger = logger)
-
