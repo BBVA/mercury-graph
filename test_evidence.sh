0e752b96e8a617d6040586a7e75dcf39761356ca
sbasaldua <santiago.basaldua@bbvadata.com>
2026-07-07 11:38:50 +0200

    Refactors test coverage report to exclude specific directories and files

--- test_evidence.sh
+++ test_evidence.sh
@@ -2,4 +2,4 @@
 
 coverage run --omit '/usr/lib/*' -m pytest unit_tests/evidence/
 
-coverage report -m
+coverage report -m --omit="mercury/graph/core/*,mercury/graph/ml/*,mercury/graph/viz/*,mercury/graph/embeddings/*,mercury/graph/__init__.py,mercury/graph/create_tutorials.py"
