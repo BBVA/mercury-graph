a56eb6ffc19873fe099cdc580f15e0034d862ab9
sbasaldua <santiago.basaldua@bbvadata.com>
2026-07-24 11:52:33 +0200

    Refactors Agent class documentation and logic to clarify upstream/downstream roles and enhance state management

--- mercury/graph/evidence/agent.py
+++ mercury/graph/evidence/agent.py
@@ -1,4 +1,4 @@
-from .agentic import Agentic
+from .agentic import Agentic, AgenticRunInvalidState, AlwaysReadyState
 
 
 class Agent(Agentic):
@@ -6,39 +6,37 @@ class Agent(Agentic):
 
 	## Overview
 
-	This has two main purposes:
+	This connects the LLM model is two ways: Upstream and Downstream.
 
-	1. Making the Agent aware of what tools it has access to, and how they work so it can create proper arguments. This is referred
-	downstream in the Agentic tree.
+	1. Upstream: It exposes the Agent's capabilities (defined in the Agents configuration) via an Agentic interface to any Agentic in
+	its Endpoint that can use it as a tool.
 
-	2. Exposing the Agentic interface to the agent in which the Agent answers very specific and well defined queries. Those queries
-	are provided by the Endpoint. The Endpoint will typically use Agents to classify user input into known categories. This  is referred
-	to as upstream in the Agentic tree.
+	2. Downstream: Making the Agent aware of what tools it has access to, and how they work so it can create proper arguments.
 
 	## Focus and Scope
 
 	Agents solve minimal problems, ideally they would be simpler models than LLMs if such models could produce correct output from inputs.
 
-	Agents are not aware of the Endpoint architecture, they only "see" what tools are below them in the Agentic tree.
+	Agents are not aware of the Endpoint architecture, they only "see" what tools have been provided to them via `add_tool()`.
 
-	Agents do not validate, prepare, plan their own tasks. The Endpoint is responsible for creating a graph of Agents (or Agentics)
-	that takes care of that.
+	Agents do not validate, prepare, plan their own tasks. The Endpoint is responsible for the "higher-level" orchestration.
 
 	These agents do not, in general, communicate through natural language. They use instructions and metadata in natural language but
-	produce structured output that may also include natural language.
+	produce structured output that may or may not include natural language.
 
 	## Interfacing with Agents
 
-#TODO: Update as in the notebook that uses tools!
+	Agents have a fixed list of capabilities. Any accounting of resources for answering a query belongs to the Endpoint. Agents do not
+	directly call a tool. Instead, they produce a litellm (OpenAI-style) answer with `finish_reason='tool_calls'` and
+	`tool_calls=[ChatCompletionMessageToolCall(function=Function(arguments='{"input": 16}', name='my_tool_for_sqrt')`. The Endpoint
+	calls the tools if the "accounting" of resources is valid and calls the Agent back with result properly appended to the Agent's
+	conversation.
 
-	Agents have a fixed list of capabilities, a fixed number of steps they can use and a list of Agentic services they can call. The first
-	thing and Agent must do is match which of its capabilities is required to answer the query. There is always a NOT_FOR_ME placeholder
-	that ends a query just as not for the Agent. A supervising Agent can check in an Agent's `meta` if the Agent should be suited to answer
-	a query.
+	## Defining an Agent
 
-	The Agent object exposes the history of the steps in the Agent's prompt. The Agent can either answer the query or ask do a query. In
-	case it does a query, the answer will be added to the history.
-
+	Agents are defined entirely by their configuration (model, capabilities and tools). Note that all capabilities in an Agent can
+	use all tools. If you want to restrict tool availability, just define more Agents. Agents are intentionally "narrow" in scope.
+	You can use as many as you want, either exposing them in the Endpoint or letting another Agent use them as a tool.
 	"""
 
 	def __init__(self, schema = None, endpoint = None, logger = None, extra_args = None):
@@ -49,14 +47,25 @@ class Agent(Agentic):
 		else:
 			self.conf = {}
 
+		self.pilot(AlwaysReadyState.READY.value)	# This forces a call to _meta() to check the configuration.
+
 
 	def _run(self, request):
-		return {'status': 'ok'}
+		raise AgenticRunInvalidState
 
 
 	def _meta(self):
-		return {'status': 'ok'}
+		meta = {'state' : AlwaysReadyState.CONSTRUCTION_FAILED.value}
+
+		if self.conf is None or 'api_base' not in self.conf or 'capabilities' not in self.conf or 'model_name' not in self.conf:
+			return meta
+
+		meta['capabilities'] = self.conf['capabilities']
+		meta['state'] = AlwaysReadyState.READY.value
+
+		return meta
 
 
 	def _dry_run(self, request):
-		return {'status': 'ok'}
+		return {'status': 1, 'description': 'Not ready.'}
+
