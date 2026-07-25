0f0a57db5188f84cafc5b543fdb4008c9edce076
sbasaldua <santiago.basaldua@bbvadata.com>
2026-07-25 20:55:40 +0200

    Refactors MyCustomGraph constructor to require 'schema' and 'extra_args' parameters, ensuring consistent signature with parent class.

--- cli/new_endpoint_template/my_e_graph.py
+++ cli/new_endpoint_template/my_e_graph.py
@@ -2,7 +2,7 @@ from mercury.graph.evidence import EvidenceGraph
 
 class MyCustomGraph(EvidenceGraph):
 
-	def __init__(self, schema = None, endpoint = None, logger = None, extra_args = None):
+	def __init__(self, schema, extra_args, endpoint = None, logger = None):
 		""" This is an example, you can inherit from: Agent, Agentic, AgenticGraph, EvidenceGraph, Formalizer, Source, or any
 			class in mercury.graph.evidence that inherits from Agentic.
 
@@ -13,4 +13,4 @@ class MyCustomGraph(EvidenceGraph):
 			You must preserve the (schema, endpoint, logger, extra_args) signature in the constructor.
 
 		"""
-		super().__init__(schema = schema, endpoint = endpoint, logger = logger, extra_args = extra_args)
+		super().__init__(schema = schema, extra_args = extra_args, endpoint = endpoint, logger = logger)
