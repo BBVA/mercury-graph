c24b6437196de96281b873d35fc52264ddac8ed2
sbasaldua <santiago.basaldua@bbvadata.com>
2026-08-11 18:47:53 +0200

    Adds a scaffold to test the newly implemented classes

---
+++ unit_tests/evidence/test_source_formats.py
@@ -0,0 +1,19 @@
+import pytest
+
+from mercury.graph.evidence.formats import PdfToMarkdown, MDbyPDFoxide, WikiMarkdownWriter
+
+
+def test_wiki_markdown_writer():
+	pass
+
+
+def test_pdf_to_markdown():
+	pass
+
+
+def test_md_by_pdfoxide():
+	pass
+
+
+# if __name__ == "__main__":
+# 	pytest.main([__file__])
---
+++ unit_tests/evidence/test_source_parts.py
@@ -0,0 +1,27 @@
+import pytest
+
+from mercury.graph.evidence import SourceNode, SourceMaker, SourceFile, SourceEntity, Chunk
+
+
+def test_source_node():
+	pass
+
+
+def test_source_maker():
+	pass
+
+
+def test_source_file():
+	pass
+
+
+def test_source_entity():
+	pass
+
+
+def test_chunk():
+	pass
+
+
+# if __name__ == "__main__":
+# 	pytest.main([__file__])
