636f824ad20c09bcdd768978e2b44dd904500029
sbasaldua <santiago.basaldua@bbvadata.com>
2026-09-07 21:50:17 +0200

    Fixes the (still TBD) test for the Agent

--- unit_tests/evidence/test_agent.py
+++ unit_tests/evidence/test_agent.py
@@ -9,17 +9,24 @@ def test_agent():
 	assert type(a) is Agent
 	assert Agent(schema = '',extra_args = {'test': True}).conf == {'test': True}
 
-	assert a.meta['state'] < 0
+	assert a.meta['state'] == a.states.INITIAL.value
 
 	with pytest.raises(AgenticRunInvalidState):
 		a.run({'cmd': 'test'})
 
-	a._dry_run({'cmd': 'test'})
+	assert a.dry_run({'cmd': 'test'}) == {'status': 0, 'description': 'Valid request.'}
 	assert type(a.meta) == dict
 
-	a = Agent(schema = 'any', extra_args = {'api_base': '', 'capabilities': ['cap1', 'cap2'], 'model_name': ''})
+	a = Agent(schema = 'any', extra_args = {
+		'description': 'Test agent.',
+		'upstream': {'name': 'test_agent', 'description': 'Test the agent.'},
+		'completion': {'model': 'test/model'}
+	})
+	a.pilot(a.states.READY.value)
 
-	assert a.meta['state'] > 0
+	assert a.meta['state'] == a.states.READY.value
+	assert a.meta['description'] == 'Test agent.'
+	assert a.meta['capabilities'][0]['function']['name'] == 'test_agent'
 
 
 # if __name__ == "__main__":
