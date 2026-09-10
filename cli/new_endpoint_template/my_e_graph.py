2d7f07d6cc1f3b4f729bc1bdee1dd7a9368ab411
sbasaldua <santiago.basaldua@bbvadata.com>
2026-09-10 20:15:01 +0200

    Refactors the MyCustomGraph to inherit from Agentic to avoid the burden of having to provide updated valid configurations

--- cli/new_endpoint_template/my_e_graph.py
+++ cli/new_endpoint_template/my_e_graph.py
@@ -1,6 +1,6 @@
-from mercury.graph.evidence import EvidenceGraph
+from mercury.graph.evidence import Agentic, AgenticRunException, AlwaysReadyState
 
-class MyCustomGraph(EvidenceGraph):
+class MyCustomGraph(Agentic):
 
 	def __init__(self, schema, extra_args, endpoint = None, logger = None):
 		""" This is an example, you can inherit from: Agent, Agentic, AgenticGraph, EvidenceGraph, Formalizer, Source, or any
@@ -12,5 +12,27 @@ class MyCustomGraph(EvidenceGraph):
 
 			You must preserve the (schema, endpoint, logger, extra_args) signature in the constructor.
 
+			Your class is responsible to provide valid extra_args to the parent when you inherit any class other than Agentic.
+
 		"""
-		super().__init__(schema = schema, extra_args = extra_args, endpoint = endpoint, logger = logger)
+		super().__init__(my_class = 'my_custom_graph', schema = schema, endpoint = endpoint, logger = logger)
+
+
+	# Cannot implement an Agentic without overriding this @abstractmethod.
+	def _run(self, request):
+
+		raise AgenticRunException
+
+
+	# Cannot implement an Agentic without overriding this @abstractmethod.
+	def _meta(self):
+		meta = {}
+		meta['state'] = AlwaysReadyState.INITIAL.value
+
+		return meta
+
+
+	# Cannot implement an Agentic without overriding this @abstractmethod.
+	def _dry_run(self, request):
+
+		return {'status': 0, 'description': 'Valid request.'}
